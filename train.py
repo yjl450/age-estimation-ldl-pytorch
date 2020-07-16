3f18201ead9b38160c9a1437c015b1e6ba410b7b
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2020-07-17 00:08:51 +0800

    Update train.py

--- train.py
+++ train.py
@@ -226,7 +226,7 @@ def main():
                     'state_dict': model_state_dict,
                     'optimizer_state_dict': optimizer.state_dict()
                 },
-                str(checkpoint_dir.joinpath("checkpoint\epoch{:03d}_{:.5f}_{:.4f}_{}_seresnext50.pth".format(epoch, val_loss, val_mae, datetime.now().strftime("%Y%m%d"))))
+                str(checkpoint_dir.joinpath("checkpoint/epoch{:03d}_{:.5f}_{:.4f}_{}_seresnext50.pth".format(epoch, val_loss, val_mae, datetime.now().strftime("%Y%m%d"))))
             )
             best_val_mae = val_mae
         else:
