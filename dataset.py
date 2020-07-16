4b2ee9b4d680446d8ae271ea23d053355e219d1e
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2020-07-16 21:54:38 +0800

    update dataset.py to fit MORPH-II

--- dataset.py
+++ dataset.py
@@ -4,18 +4,20 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import torch
-import cv2
+from PIL import Image
 from torch.utils.data import Dataset
 from imgaug import augmenters as iaa
+import torchvision.transforms as transforms
 
 
 class ImgAugTransform:
     def __init__(self):
         self.aug = iaa.Sequential([
             iaa.OneOf([
-                iaa.Sometimes(0.25, iaa.AdditiveGaussianNoise(scale=0.1 * 255)),
+                iaa.Sometimes(
+                    0.25, iaa.AdditiveGaussianNoise(scale=0.1 * 255)),
                 iaa.Sometimes(0.25, iaa.GaussianBlur(sigma=(0, 3.0)))
-                ]),
+            ]),
             iaa.Affine(
                 rotate=(-20, 20), mode="edge",
                 scale={"x": (0.95, 1.05), "y": (0.95, 1.05)},
@@ -34,12 +36,18 @@ class ImgAugTransform:
 
 class FaceDataset(Dataset):
     def __init__(self, data_dir, data_type, img_size=224, augment=False, age_stddev=1.0):
-        assert(data_type in ("train", "valid", "test"))
-        csv_path = Path(data_dir).joinpath(f"gt_avg_{data_type}.csv")
-        img_dir = Path(data_dir).joinpath(data_type)
+        assert(data_type in ("train", "valid"))
+        csv_path = Path(data_dir).joinpath(f"Morph_{data_type}_align.csv")
+        img_dir = Path(data_dir)
         self.img_size = img_size
         self.augment = augment
         self.age_stddev = age_stddev
+        self.transform = transforms.Compose([
+            transforms.Resize(img_size),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[
+                                 0.247, 0.243, 0.261]),
+        ])
 
         if augment:
             self.transform = ImgAugTransform()
@@ -48,22 +56,21 @@ class FaceDataset(Dataset):
 
         self.x = []
         self.y = []
-        self.std = []
+        # self.std = []
+        self.rotate = []
+        self.boxes = []
         df = pd.read_csv(str(csv_path))
-        ignore_path = Path(__file__).resolve().parent.joinpath("ignore_list.csv")
-        ignore_img_names = list(pd.read_csv(str(ignore_path))["img_name"].values)
 
         for _, row in df.iterrows():
-            img_name = row["file_name"]
-
-            if img_name in ignore_img_names:
-                continue
-
-            img_path = img_dir.joinpath(img_name + "_face.jpg")
+            img_name = row["photo"]
+            img_path = img_dir.joinpath(img_name)
             assert(img_path.is_file())
             self.x.append(str(img_path))
-            self.y.append(row["apparent_age_avg"])
-            self.std.append(row["apparent_age_std"])
+            self.y.append(row["age"])
+            self.rotate.append(row["deg"])
+            self.boxes.append(
+                [row["box1"], row["box2"], row["box3"], row["box4"]])
+            # self.std.append(row["apparent_age_std"])
 
     def __len__(self):
         return len(self.y)
@@ -72,26 +79,26 @@ class FaceDataset(Dataset):
         img_path = self.x[idx]
         age = self.y[idx]
 
-        if self.augment:
-            age += np.random.randn() * self.std[idx] * self.age_stddev
-
-        img = cv2.imread(str(img_path), 1)
-        img = cv2.resize(img, (self.img_size, self.img_size))
-        img = self.transform(img).astype(np.float32)
-        return torch.from_numpy(np.transpose(img, (2, 0, 1))), np.clip(round(age), 0, 100)
+        # if self.augment:
+        #     age += np.random.randn() * self.std[idx] * self.age_stddev
+        img = Image.open(img_path)
+        # img.show()
+        img = img.rotate(
+            self.rotate[idx], resample=Image.BICUBIC, expand=True)  # Alignment
+        img = img.crop(self.boxes[idx])
+        # img.show()
+        return self.transform(img), age #np.clip(round(age), 0, 100)
 
 
 def main():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--data_dir", type=str, required=True)
     args = parser.parse_args()
     dataset = FaceDataset(args.data_dir, "train")
     print("train dataset len: {}".format(len(dataset)))
     dataset = FaceDataset(args.data_dir, "valid")
     print("valid dataset len: {}".format(len(dataset)))
-    dataset = FaceDataset(args.data_dir, "test")
-    print("test dataset len: {}".format(len(dataset)))
-
 
 if __name__ == '__main__':
     main()
