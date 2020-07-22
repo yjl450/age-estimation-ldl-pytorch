a5d7d5e9c3a5e2a8d3b3b99e7cc005a9f34736af
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2020-07-23 01:20:10 +0800

    change normalization value

--- dataset.py
+++ dataset.py
@@ -45,8 +45,8 @@ class FaceDataset(Dataset):
         self.transform = transforms.Compose([
             transforms.Resize((img_size, img_size)),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[
-                                 0.247, 0.243, 0.261]),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
+                                 0.229, 0.224, 0.225])
         ])
 
         # if augment:
