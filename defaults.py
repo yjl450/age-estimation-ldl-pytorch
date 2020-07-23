b71d127aaf9be076a1b03b23bd1cff6e4d1f6fd5
yjl450 <yjl450@nyu.edu>
2020-07-23 12:47:47 -0400

    modified:   defaults.py 	new file:   savefig/Morph_train_accu.png 	new file:   savefig/Morph_train_loss.png 	new file:   savefig/Morph_val_accu.png 	new file:   savefig/Morph_val_loss.png 	new file:   savefig/megaage_train_accu.png 	new file:   savefig/megaage_train_loss.png 	new file:   savefig/megaage_val_accu.png 	new file:   savefig/megaage_val_loss.png

--- defaults.py
+++ defaults.py
@@ -4,7 +4,7 @@ _C = CN()
 
 # Model
 _C.MODEL = CN()
-_C.MODEL.ARCH = "se_resnext50_32x4d"  # check python train.py -h for available models
+_C.MODEL.ARCH = "vgg16_bn"  # check python train.py -h for available models
 _C.MODEL.IMG_SIZE = 224
 
 # Train
