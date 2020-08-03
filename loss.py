724b4dc99580468e831abb6f35b1a7fa175f467f
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2020-08-04 01:38:51 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -3,10 +3,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 def kl_loss(inputs, labels):
-    criterion = nn.KLDivLoss(reduce=False)
-    outputs = torch.log(inputs)
+    criterion = nn.KLDivLoss(reduction="batchmean")
+    outputs = F.log_softmax(inputs, dim=1)
+    # print(outputs)
+    # outputs = torch.log(outputs)
     loss = criterion(outputs, labels)
-    loss = loss.sum()/loss.shape[0] # batch average loss
+    # loss = loss.sum()/loss.shape[0] # batch average loss
     return loss
 
 def L1_loss(inputs, labels):
