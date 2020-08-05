6188ab6eccebf7a7b7052a1862b65a38fa4d2f7c
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2020-08-06 01:32:53 +0800

    add models

--- defaults.py
+++ defaults.py
@@ -24,3 +24,14 @@ _C.TRAIN.AGE_STDDEV = 1.0
 _C.TEST = CN()
 _C.TEST.WORKERS = 8
 _C.TEST.BATCH_SIZE = 128
+
+
+# available models: ['alexnet', 'bninception', 'cafferesnet101', 'densenet121',
+# 'densenet161', 'densenet169', 'densenet201', 'dpn107', 'dpn131', 'dpn68',
+# 'dpn68b', 'dpn92', 'dpn98', 'fbresnet152', 'inceptionresnetv2', 'inceptionv3',
+# 'inceptionv4', 'nasnetalarge', 'nasnetamobile', 'pnasnet5large', 'polynet',
+# 'resnet101', 'resnet152', 'resnet18', 'resnet34', 'resnet50',
+# 'resnext101_32x4d', 'resnext101_64x4d', 'se_resnet101', 'se_resnet152',
+# 'se_resnet50', 'se_resnext101_32x4d', 'se_resnext50_32x4d', 'senet154',
+# 'squeezenet1_0', 'squeezenet1_1', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn',
+# 'vgg16', 'vgg16_bn', 'vgg19', 'vgg19_bn', 'xception']
