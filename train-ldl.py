ff044d3f0303efea41bb2e26cd9e41245bbb920f
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2020-08-11 20:59:24 +0800

    remove debug info

--- train-ldl.py
+++ train-ldl.py
@@ -102,9 +102,6 @@ def train(train_loader, model, criterion, optimizer, epoch, device):
             # calc loss
             # loss = criterion(outputs, y)
             loss1 = L.kl_loss(outputs, lbl)
-            if torch.isnan(loss1) or torch.isinf(loss1):
-                pass
-            # print("Train loss1", loss1.item())
             loss2 = L.L1_loss(ages, y)
             loss = loss1 + loss2
             cur_loss = loss.item()
