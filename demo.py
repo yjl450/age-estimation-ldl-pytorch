b1ccda5e6cac71481633db79ab1846ae32fe609b
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2020-08-12 20:50:10 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,6 +3,7 @@ import better_exceptions
 from pathlib import Path
 from contextlib import contextmanager
 import urllib.request
+import facenet_pytorch
 import numpy as np
 import cv2
 import dlib
@@ -14,13 +15,16 @@ import torch.utils.data
 import torch.nn.functional as F
 from model import get_model
 from defaults import _C as cfg
+from time import perf_counter
+from facenet_pytorch import MTCNN
+from PIL import Image
 
 def get_args():
     parser = argparse.ArgumentParser(description="Age estimation demo",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("--resume", type=str, default=None,
                         help="Model weight to be tested")
-    parser.add_argument("--margin", type=float, default=0.4,
+    parser.add_argument("--margin", type=float, default=0,
                         help="Margin around detected face for age-gender estimation")
     parser.add_argument("--img_dir", type=str, default=None,
                         help="Target image directory; if set, images in image_dir are used instead of webcam")
@@ -77,6 +81,12 @@ def yield_images_from_dir(img_dir):
             yield cv2.resize(img, (int(w * r), int(h * r))), img_path.name
 
 
+def angel(t1, t2):
+    x = t2[0]-t1[0]
+    y = (t2[1]-t1[1])
+    return math.degrees(math.atan2(y, x))
+
+
 def main():
     args = get_args()
 
@@ -125,47 +135,60 @@ def main():
     margin = args.margin
     img_dir = args.img_dir
     detector = dlib.get_frontal_face_detector()
+    mtcnn = MTCNN(device=device, post_process=False)
     img_size = cfg.MODEL.IMG_SIZE
     image_generator = yield_images_from_dir(img_dir) if img_dir else yield_images()
     rank = torch.Tensor([i for i in range(101)]).to(device)
 
     with torch.no_grad():
         for img, name in image_generator:
+            start = perf_counter()
             input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            img_h, img_w, _ = np.shape(input_img)
+            image = Image.fromarray(input_img)
+            detected, probs = mtcnn.detect(image)
 
-            # detect faces using dlib detector
-            detected = detector(input_img, 1)
-            faces = np.empty((len(detected), img_size, img_size, 3))
+            img_h, img_w = image.size
 
-            if len(detected) > 0:
+            # # detect faces using dlib detector
+            # detected = detector(input_img, 1)
+            # print(detected)
+
+            if detected is not None and len(detected) > 0:
+                faces = torch.zeros((len(detected), 3, img_size, img_size))
                 for i, d in enumerate(detected):
-                    x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
+                    # x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
+                    x1, y1, x2, y2, w, h = d[0], d[1], d[2], d[3], d[2]-d[0], d[3]-d[1]
                     xw1 = max(int(x1 - margin * w), 0)
                     yw1 = max(int(y1 - margin * h), 0)
                     xw2 = min(int(x2 + margin * w), img_w - 1)
                     yw2 = min(int(y2 + margin * h), img_h - 1)
                     cv2.rectangle(img, (x1, y1), (x2, y2), (255, 255, 255), 2)
                     cv2.rectangle(img, (xw1, yw1), (xw2, yw2), (255, 0, 0), 2)
-                    faces[i] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1], (img_size, img_size))
-
-                # predict ages
-                inputs = torch.from_numpy(np.transpose(faces.astype(np.float32), (0, 3, 1, 2))).to(device)
-                outputs = F.softmax(model(inputs), dim=-1)
+                    faces[i] = torch.Tensor(cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1], (img_size, img_size))).permute(2, 0, 1)
+                print(faces.shape)
+            #     # predict ages
+                outputs = model(faces.to(device))
+                outputs = F.softmax(outputs, dim=-1)
                 if args.ldl:
                     predicted_ages = torch.sum(outputs * rank, dim = -1)
                 else:
                     _, predicted_ages = outputs.max(1)
+                print(predicted_ages)
 
-                # draw results
-                for i, d in enumerate(detected):
-                    label = "{}".format(int(predicted_ages[i]))
-                    draw_label(img, (d.left(), d.top()), label)
+            # draw results
+            # for i, d in enumerate(detected):
+            #     label = "{}".format(int(predicted_ages[i]))
+            #     draw_label(img, (d.left(), d.top()), label)
+
+            # faces = np.array(faces.permute(1, 2, 0)).astype(np.uint8)
+            # faces = cv2.cvtColor(faces, cv2.COLOR_RGB2BGR)
 
             if args.output_dir is not None:
                 output_path = output_dir.joinpath(name)
                 cv2.imwrite(str(output_path), img)
             else:
+                elapsed = perf_counter() - start
+                cv2.putText(img, "FPS: "+ "{:.1f}".format(60/elapsed),(10,20),cv2.FONT_HERSHEY_DUPLEX,0.5,(255, 255,255),1)
                 cv2.imshow("result", img)
                 key = cv2.waitKey(-1) if img_dir else cv2.waitKey(30)
 
