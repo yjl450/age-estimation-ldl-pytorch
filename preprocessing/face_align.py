275891ecf1fb4bb0178936417bf57662491a265c
Yijian Liu <32358997+yjl450@users.noreply.github.com>
2020-09-01 00:05:42 +0800

    add preprocessing script sample

---
+++ preprocessing/face_align.py
@@ -0,0 +1,67 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+
+# %%
+from facenet_pytorch import MTCNN
+from PIL import Image
+import math
+import pandas as pd
+import csv
+from PIL import ImageDraw
+from torchvision.transforms import ToPILImage, ToTensor
+from tqdm import tqdm
+
+
+# %%
+mtcnn = MTCNN(device="cuda")
+
+
+# %%
+def angel(t1, t2):
+    x = t2[0]-t1[0]
+    y = (t2[1]-t1[1])
+    return math.degrees(math.atan2(y, x))
+
+
+# %%
+path = ""
+mode = ["train", "test"]
+for i in mode:
+    f = open(path + "MegaAgeAsian_{}_align.csv".format(i), "w", newline='')
+    writer = csv.writer(f)
+
+    with open(path + 'MegaAgeAsian_{}.csv'.format(i),'r') as csvfile:
+        header = next(csvfile).split(",")
+        header[-1] = "box4"
+        writer.writerow(header)
+        reader = csv.reader(csvfile)
+        # pbar = tqdm(total = 55134)
+        for row in tqdm(reader):
+            # pbar.update(1)
+            img = Image.open(path + row[0])
+
+            # display(img)
+
+            boxes, probs, landmarks = mtcnn.detect(img, landmarks=True) # Get eye position for alignment
+            deg = angel(landmarks[0][0], landmarks[0][1])
+            img = img.rotate(deg, resample=Image.BICUBIC, expand=True)
+            boxes, probs, landmarks = mtcnn.detect(img, landmarks=True) # Get bounding box
+            # img1 = ImageDraw.Draw(img)
+            # img1.rectangle(boxes, outline="red", width=2)
+            # img1.point(landmarks[0][0], fill="red") #left eye
+            # img1.point(landmarks[0][1], fill="green") #right eye
+            # img1.point(landmarks[0][2], fill="blue")
+            # img1.point(landmarks[0][3], fill="yellow")
+            # img1.point(landmarks[0][4], fill="orange")
+            # display(img)
+            writer.writerow(row+[str(deg)]+list(boxes[0]))
+
+            # break
+    # pbar.close
+    f.close()
+print("done")
+
+
+# %%
+
+
---
+++ preprocessing/generate_csv.py
@@ -0,0 +1,47 @@
+import csv
+
+path = "list/"
+
+train_align = open("MegaAgeAsian_train_align.csv", "w", newline='')
+valid_align = open("MegaAgeAsian_valid_align.csv", "w", newline='')
+header = ["photo", "age", "deg", "box1", "box2", "box3", "box4"]
+writer1 = csv.writer(train_align)
+writer2 = csv.writer(valid_align)
+writer1.writerow(header)
+writer2.writerow(header)
+
+# train csv
+mode = "train"
+
+f_name = open(path+ mode +"_name.txt", "r")
+f_age = open(path+ mode+ "_age.txt", "r")
+name_data = f_name.readlines()
+age_data = f_age.readlines()
+lines = len(name_data)
+print(lines)
+for i in range(lines):
+    to_write = []
+    to_write.append(mode+"/"+name_data[i].rstrip())
+    to_write.append(age_data[i].rstrip())
+    writer1.writerow(to_write)
+f_name.close()
+f_age.close()
+
+# test csv
+mode = "test"
+
+f_name = open(path+ mode +"_name.txt", "r")
+f_age = open(path+ mode+ "_age.txt", "r")
+name_data = f_name.readlines()
+age_data = f_age.readlines()
+lines = len(name_data)
+for i in range(lines):
+    to_write = []
+    to_write.append(mode+"/"+name_data[i].rstrip())
+    to_write.append(age_data[i].rstrip())
+    writer2.writerow(to_write)
+f_name.close()
+f_age.close()
+
+train_align.close()
+valid_align.close()
